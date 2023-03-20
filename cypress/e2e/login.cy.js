baffd0742a52a31b1e97d5b48a1b06420f01809a
Eduardo Lemos <ed.lemos@live.com>
2023-03-20 13:59:30 -0300

    automação básica de login

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress" /> 
+
+context('Funcionalidade de login', () => {
+    
+    it('Login realizado com sucesso', () => {
+        
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.page-title').should('contain', 'Minha conta')
+    
+    });
+
+    it('Login com usuario invalido', () => {
+
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('alunos_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+        
+    });
+
+    it('Login com senha invalida', () => {
+
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com.br')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: a senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+        
+    });
+});
