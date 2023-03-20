bb6f8669853a78770b083e401a3e4a6b67d6f800
Eduardo Lemos <ed.lemos@live.com>
2023-03-20 16:49:10 -0300

    teste de lista de produtos

--- cypress/e2e/preCadastro.cy.js
+++ cypress/e2e/preCadastro.cy.js
@@ -8,13 +8,18 @@ describe('Funcionalidade de pré cadastro', () => {
     });
     
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email())
+
+        let nomeFaker = faker.name.firstName()
+        let sobreNomeFaker = faker.name.lastName()
+        let emailFaker = faker.internet.email(nomeFaker)
+
+        cy.get('#reg_email').type(emailFaker)
         cy.get('#reg_password').type(faker.internet.password())
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.name.firstName());
-        cy.get('#account_last_name').type(faker.name.lastName());
+        cy.get('#account_first_name').type(nomeFaker);
+        cy.get('#account_last_name').type(sobreNomeFaker);
         cy.get('.woocommerce-Button').click();
 
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso');        
